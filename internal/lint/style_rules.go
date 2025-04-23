5d0f9826b8ed4fffc4d7608c5e17cd30bed9e0b7
ktierney15 <kevintierney99@gmail.com>
2025-04-23 16:41:47 -0400

    create performance rules file

--- internal/lint/style_rules.go
+++ internal/lint/style_rules.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"strings"
 	"regexp"
-	"encoding/json"
 )
 
 func ValidJobNames(data map[string]interface{}) (*bool, string) {
@@ -108,50 +107,4 @@ func UsingActionVersion(data map[string]interface{}) (*bool, string) {
 
 
 	return &isUsingOnlyVersions, strings.TrimSuffix(failureOutputMessage, ", ")
-}
-
-
-func RedundantSteps(data map[string]interface{}) (*bool, string) {
-	// checks if there are any duplicate steps in a job
-	noRedundantSteps := true
-	failureOutputMessage := ""
-
-	jobField, ok := data["jobs"].(map[string]interface{})
-	if !ok {
-	}
-
-	for jobName, jobValue := range jobField {
-		jobMap , ok:= jobValue.(map[string]interface{})
-		if !ok {
-			continue
-		}
-
-		steps, ok := jobMap["steps"].([]interface{})
-		if !ok {
-			continue
-		}
-
-		seenSteps := make(map[string]bool)
-
-		for _, step := range steps {
-			stepMap, ok := step.(map[string]interface{})
-			if !ok {
-				continue
-			}
-			stepBytes, err := json.Marshal(stepMap)
-			if err != nil {
-				continue
-			}
-
-			stepKey := string(stepBytes)
-			if seenSteps[stepKey] {
-				noRedundantSteps = false
-				failureOutputMessage += fmt.Sprintf("Job '%s' has a redundant step: %s, ", jobName, stepKey)
-			} else {
-				seenSteps[stepKey] = true
-			}
-		}
-	}
-
-	return &noRedundantSteps, strings.TrimSuffix(failureOutputMessage, ", ")
 }
